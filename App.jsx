2aea181bfa69b14324e8d486915cdd62ae5cb4fa
hibiki <hibiki1068@gmail.com>
2021-02-11 07:24:09 +0900

    add Edit and Creat Screens

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import MemoCreateScreen from './src/screens/MemoCreate';
 import MemodetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
-    <MemodetailScreen />
+    // <MemodetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
