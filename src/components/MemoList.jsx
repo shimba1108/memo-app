d4a3297cfdf97fc936a04ba996459407fbd5f9a9
hibiki <hibiki1068@gmail.com>
2021-02-16 03:36:17 +0900

    add delete function

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -12,12 +12,35 @@ import {
   StyleSheet,
   TouchableOpacity,
   FlatList,
+  Alert,
 } from 'react-native';
+import firebase from 'firebase';
 import { dateToString } from '../utils';
 
 export default function MemoList(props) {
   const { memos } = props;
   const navigation = useNavigation();
+
+  function deleteMemo(id) {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      Alert.alert('Delete memo', 'Are you sure?', [
+        {
+          text: 'Cancel',
+          onPress: () => {},
+        },
+        {
+          text: 'Delete',
+          style: 'destructive',
+          onPress: () => {
+            ref.delete().catch(() => { Alert.alert('Failed Delete'); });
+          },
+        },
+      ]);
+    }
+  }
 
   function renderItem({ item }) {
     return (
@@ -30,7 +53,10 @@ export default function MemoList(props) {
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
           <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
-        <TouchableOpacity style={styles.memoDelete}>
+        <TouchableOpacity
+          style={styles.memoDelete}
+          onPress={() => { deleteMemo(item.id); }}
+        >
           <Text style={styles.memoListItemDelete}>✕</Text>
         </TouchableOpacity>
       </TouchableOpacity>
