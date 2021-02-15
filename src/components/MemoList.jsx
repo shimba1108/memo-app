37c032506a5799c59cc1de42bdc3f56cbdef103b
hibiki <hibiki1068@gmail.com>
2021-02-15 23:20:33 +0900

    Fetch data from firestore and pass it FlatList

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,34 +1,64 @@
 import { useNavigation } from '@react-navigation/native';
+import {
+  arrayOf,
+  instanceOf,
+  shape,
+  string,
+} from 'prop-types';
 import React from 'react';
 import {
-  ScrollView,
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 
-export default function MemoList() {
+export default function MemoList(props) {
+  const { memos } = props;
   const navigation = useNavigation();
-  return (
-    <ScrollView>
+
+  function renderItem({ item }) {
+    return (
       <TouchableOpacity
+        key={item.id}
         style={styles.memoListItem}
         onPress={() => { navigation.navigate('MemoDetail'); }}
       >
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020/01/01</Text>
+          <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity style={styles.memoDelete}>
           <Text style={styles.memoListItemDelete}>✕</Text>
         </TouchableOpacity>
       </TouchableOpacity>
-    </ScrollView>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={memos}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
   );
 }
 
+MemoList.propTypes = {
+  memos: arrayOf(shape({
+    id: string,
+    bodyText: string,
+    updatedAt: instanceOf(Date),
+  })).isRequired,
+};
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   memoListItem: {
     backgroundColor: '#ffffff',
     flexDirection: 'row',
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
+import firebase from 'firebase';
 import AddButton from '../components/AddButton';
 import LogOutButton from '../components/LogOutButton';
 import MemoList from '../components/MemoList';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogOutButton />,
     });
   }, []);
 
+  useEffect(() => {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
+      unsubscribe = ref.onSnapshot((snapshot) => {
+        const userMemo = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          userMemo.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updatedAt: data.updatedAt.toDate(),
+          });
+        });
+        setMemos(userMemo);
+      }, (error) => {
+        console.log(error);
+        Alert.alert('データの読み込みに失敗しました');
+      });
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MemoList />
+      <MemoList memos={memos} />
       <AddButton
         name="plus"
         onPress={() => { navigation.navigate('MemoCreator'); }}
